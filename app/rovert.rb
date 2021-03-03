a893a81a15969d7887b1ea057f62847372876b1a
Jose Luis <joseluis2lopez@gmail.com>
2021-03-03 17:00:54 +0100

    Add move function to Rovert class

--- app/rovert.rb
+++ app/rovert.rb
@@ -3,6 +3,8 @@ class Rovert
   attr_reader :y
   attr_reader :direction
 
+  ALLOW_MOVEMENTS = ['L','R','M']
+
   def initialize(position, plateau)
     @x = position.split[0].to_i
     @y = position.split[1].to_i
@@ -10,4 +12,13 @@ class Rovert
     @plateau = plateau
   end
 
+  def move(movements)
+    movements.split.each do |movement|
+      if ALLOW_MOVEMENTS.include? movement
+        movement == 'M' ? forward : turn(movement)
+      else
+        puts 'ERROR: Movement instruction unknown'
+      end
+    end
+  end
 end
